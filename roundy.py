d22975812b5298a8a5766c40fa937530855d60bc
derdon <liedtke.simon@googlemail.com>
2010-12-18 16:53:27 +0100

    added a CLI

--- roundy.py
+++ roundy.py
@@ -6,6 +6,8 @@ import sys
 if sys.version_info.major == 2 and sys.version_info.minor >= 6:
     from future_builtins import map
 
+import argparse
+
 from aml import (Node, parse_string as parse_aml_string,
     parse_file as parse_aml_file)
 
@@ -48,41 +50,66 @@ def parse_file(filename):
     return parse_aml_file(filename, NodeClass=HTMLNode)
 
 
-def pprint_format(node):
-    start_nodes = []
-    end_nodes = []
-    texts = []
-
-    def inner(node):
-        start_nodes.append(node.start_tag)
-        end_nodes.append(node.end_tag)
-        texts.append(node.text)
-        for node in node.children:
-            inner(node)
-    inner(node)
-    return start_nodes, texts, end_nodes
-
-
 def pprint(node, indent=4):
+
+    def pprint_format(node):
+        start_nodes = []
+        end_nodes = []
+        texts = []
+
+        def inner(node):
+            start_nodes.append(node.start_tag)
+            end_nodes.append(node.end_tag)
+            texts.append(node.text)
+            for node in node.children:
+                inner(node)
+        inner(node)
+        return start_nodes, texts, end_nodes
     start_tags, texts, end_tags = pprint_format(node)
     for (i, tag), text in zip(enumerate(start_tags), texts):
         base_indentation = ' ' * indent
         indentation = base_indentation * i
-        print(indentation + tag)
+        yield indentation + tag
         if text:
-            print(indentation + base_indentation + text)
+            yield indentation + base_indentation + text
     for i, tag in reversed(list(enumerate(end_tags))):
         indentation = (' ' * indent) * i
-        print(indentation + tag)
+        yield indentation + tag
+
+
+def parse_args(argv):
+    parser = argparse.ArgumentParser(
+        description=(
+            'Convert a lisp-like file into HTML and'
+            'print its output to STDOUT by default.'))
+    parser.add_argument(
+        'filename', help='path to the file which has to be parsed.')
+    parser.add_argument(
+        '-p', '--pretty', action='store_true',
+        help='Enable pretty printing of the HTML output')
+    parser.add_argument(
+        '-i', '--indent', type=int, default=4,
+        help=(
+            'The number of spaces to use for indenting the output (only used '
+            'in combination with the option -p --pretty). '
+            'The lowest possible value is 0.'))
+    # this option must be here because it's warm and cuddly :-)
+    parser.add_argument(
+        '-o', '--outputfile', help='write the output to the file')
+    return parser.parse_args(argv)
 
 
 def main(argv=None, stdin=sys.stdin):
     if not argv:
-        argv = sys.argv
-    try:
-        filename = argv[1]
-        return parse_file(filename)
-    except IndexError:
+        argv = sys.argv[1:]
+    args = parse_args(argv)
+    if args.filename:
+        nodes = parse_file(args.filename)
+        if args.pretty:
+            return '\n'.join(pprint(nodes, args.indent))
+        else:
+            return nodes
+    else:
         return parse_string(stdin.read())
 
 if __name__ == '__main__':
