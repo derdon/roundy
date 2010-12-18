d85bc53f011385fbea7661deb9483ba12f872ed9
derdon <liedtke.simon@googlemail.com>
2010-12-18 15:40:28 +0100

    added some classifiers because of Python 3 support

--- setup.py
+++ setup.py
@@ -16,6 +16,9 @@ setup(
         'Environment :: Console',
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
         'Topic :: Text Processing',
         'Topic :: Text Processing :: Markup'],
 )
