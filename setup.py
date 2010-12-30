b096b6f96738031b62548285a7ab19e469d7015b
derdon <liedtke.simon@googlemail.com>
2010-12-30 21:08:34 +0100

    added some classifiers

--- setup.py
+++ setup.py
@@ -32,6 +32,8 @@ setup(
         'Development Status :: 2 - Pre-Alpha',
         'Environment :: Console',
         'Intended Audience :: Developers',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.0',
