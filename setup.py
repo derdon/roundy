4f379c33528fcba8b1e0ab4772281705b1eb09ec
derdon <liedtke.simon@googlemail.com>
2011-04-30 18:55:44 +0200

    plan supporting the python versions 2.5, 2.6, 3.0 and 3.2

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ import sys
 
 from setuptools import setup
 
-if sys.version_info < (2, 6):
+if sys.version_info < (2, 5):
     sys.exit(
         "Fatal Error: couldn't install roundy. Reason: Python version is too "
-        "low (Python2.6 or higher is required)")
+        "low (Python2.5 or higher is required)")
 
 # copied from
 # http://lucumr.pocoo.org/2010/2/11/porting-to-python-3-a-guide/
@@ -15,8 +15,8 @@ if sys.version_info >= (3, 0):
         use_2to3=True,
         use_2to3_fixers=['custom_fixers'])
 
-requirements = ['aml', 'distribute']
-needs_argparse = sys.version_info[:2] == (2, 6) or (3, 1)
+requirements = ['distribute', 'aml']
+needs_argparse = sys.version_info[:2] in [(2, 5), (2, 6), (3, 0), (3, 1)]
 if needs_argparse:
     requirements += ['argparse']
 
@@ -33,15 +33,18 @@ setup(
     scripts=['roundy.py'],
     install_requires=requirements,
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 - Alpha',
         'Environment :: Console',
         'Intended Audience :: Developers',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.0',
         'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
         'Topic :: Text Processing',
         'Topic :: Text Processing :: Markup'],
     **extra
